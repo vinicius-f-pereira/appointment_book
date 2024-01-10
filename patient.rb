80a9bdab98db3ec8644ba131e3399d3f61bd90e1
vde-frei <vde-frei@student.42sp.org.br>
2024-01-10 14:47:19 -0300

    update gem files

--- patient.rb
+++ patient.rb
@@ -8,17 +8,17 @@ def add_patient(patients)
 	name = gets.chomp
 	print "Telefone do paciente: "
 	phone = gets.chomp
-  
+
 	if name.empty? || phone.empty?
 	  puts "Nome e telefone são obrigatórios."
 	  return
 	end
 
 	unless phone.match?(/\A\+?\d{1,3}\s?\d{1,3}\s?\d{1,5}\s?\-?\d{1,4}\z/)
-	  puts "Formato de telefone inválido. Use o formato +999999999."
+	  puts "Formato de telefone inválido. Use o formato +55 11 9876 9543."
 	  return
 	end
-  
+
 	if patients[phone]
 	  puts "Paciente já cadastrado!"
 	else
@@ -26,6 +26,3 @@ def add_patient(patients)
 	  puts "Paciente cadastrado com sucesso"
 	end
 end
-
-
- 
